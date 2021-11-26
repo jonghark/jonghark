fac45ee465c6b62c52d631eacd369d0253a8c723
박종학 <jonghark87@smartix.co.kr>
2021-11-26 17:35:22 +0900

    commit

--- src/main/java/com/server/web/ServerController.java
+++ src/main/java/com/server/web/ServerController.java
@@ -2,6 +2,7 @@ package com.server.web;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
