0d1c41a1c78695013431a65e64cf778ffdf609b0
john <jonghark87@gmail.com>
2023-05-10 16:49:22 +0900

    commit

--- src/main/java/examples/stream/Example_stream.java
+++ src/main/java/examples/stream/Example_stream.java
@@ -3,6 +3,7 @@ package examples.stream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.stream.Collectors;
 
 public class Example_stream {
     public static void main(String[] arg){
@@ -12,5 +13,12 @@ public class Example_stream {
             int num = iterator.next();
             System.out.println("===>" + num);
         }
+
+        String[] str = new String[]{"a","b","c"};
+        Arrays.stream(str).collect(Collectors.joining());
+        //a,b,c
+        int[] num_list = new int[]{3, 4, 5, 2, 5, 4, 6, 7, 3, 7, 2, 2, 1};
+        Arrays.stream(num_list).reduce(1,(x,y) ->x*y); //전체 곱하기 
+        Arrays.stream(num_list).sum();//전체 더하기
     }
 }
