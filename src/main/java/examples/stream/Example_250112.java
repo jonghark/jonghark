1f16fb0bf2eca0742737bb4800290a8f9d66c8fb
jonghark <jonghark87@gmail.com>
2025-01-12 19:46:02 +0900

    -commit

---
+++ src/main/java/examples/stream/Example_250112.java
@@ -0,0 +1,13 @@
+package examples.stream;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Example_250112 {
+    public static void main(String arg[]) {
+
+        String a = "AbCdEfG";
+        String b = "aBc";
+
+    }
+}
